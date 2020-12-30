5f498c0cf60ae0720bc3039101613f213f9c4d56
Lee JungHyeok <wndgur76@naver.com>
2020-12-30 23:53:52 +0900

    디자인 요소 추가. 눈알, 검은자 크기에 따른 속도제한 추가

--- Reptopia/Design/eyeFollow.js
+++ Reptopia/Design/eyeFollow.js
@@ -9,32 +9,39 @@ document.body.appendChild(canvasEl);
 canvasEl.addEventListener("pointermove", mouseMove);
 canvasEl.addEventListener("pointerup", mouseMove);
 
-eye1 = new Eye(5, 50, "black", "white");
+var eyes = [];
 
+frame = 0;
 loop();
 
 //부드러운 구현 (dx, dy 추가해야할듯?)
 
-function Eye(radian1, radian2, color1, color2){
-    this.x = canvasEl.width/2;
-    this.y = canvasEl.height/2;
+function Eye(x, y, radian1, radian2, color1, color2){
+    this.x = x;
+    this.y = y;
     this.pinX = this.x;
     this.pinY = this.y;
 
     this.dx = 0;
     this.dy = 0;
 
-    this.r1 = radian1;
+    if(radian1>radian2*3/4) this.r1 = radian2*3/4;
+    else this.r1 = radian1;
     this.col1 = color1;
 
     this.r2 = radian2;
     this.col2 = color2;
 
-    this.v = this.r2/2000;
+    this.v = (this.r2 - this.r1)/1200 + 0.0001;
 
     this.look = function(aimX, aimY){
         this.dx = (((this.r2-this.r1)-distance([this.x, this.y], [this.pinX, this.pinY]))/(this.r2-this.r1)) * (aimX - this.x) * this.v;
         this.dy = (((this.r2-this.r1)-distance([this.x, this.y], [this.pinX, this.pinY]))/(this.r2-this.r1)) * (aimY - this.y) * this.v;
+
+        if(this.dx > this.r2) this.dx /= 2;
+        if(this.dx < -this.r2) this.dx /= 2;
+        if(this.dy > this.r2) this.dy /= 2;
+        if(this.dy < -this.r2) this.dy /= 2;
 
         this.x += this.dx;
         this.y += this.dy;
@@ -53,26 +60,36 @@ function Eye(radian1, radian2, color1, color2){
     }
 
     this.toCenter = function(){
-        this.x += (this.pinX-this.x)/300;
-        this.y += (this.pinY-this.y)/300;
+        this.x += (this.pinX-this.x)/200;
+        this.y += (this.pinY-this.y)/200;
     }
 }
 
 function loop(){
+    frame++;
+
+    if(eyes.length<300) eyes.push(randomEye());
+    if(frame%500==0) eyes.splice(0, eyes.length-1);
 
     ctx.fillStyle = "gray";
     ctx.fillRect(0, 0, canvasEl.width, canvasEl.height);
 
-    eye1.draw();
-    eye1.toCenter();
+    eyes.forEach(function(obj){obj.draw();});
+    eyes.forEach(function(obj){obj.toCenter();});
 
     requestAnimationFrame(loop);
 };
 
 function mouseMove(event){
-    eye1.look(event.pageX-9, event.pageY-90);
+    eyes.forEach(function(eye){eye.look(event.pageX-9, event.pageY-90);});
 }
 
 function distance(location1, location2){
     return Math.sqrt(((location1[0]-location2[0])**2 + (location1[1] - location2[1])**2));
+}
+
+function randomEye(){
+    let rndRGB1 = "rgb(" + Math.random()*255 + "," + Math.random()*255 + "," + Math.random()*255 + ")";
+    let rndRGB2 = "rgb(" + Math.random()*255 + "," + Math.random()*255 + "," + Math.random()*255 + ")";
+    return new Eye(Math.random()*canvasEl.width, Math.random()*canvasEl.height, Math.random()*canvasEl.height/30 + 10, Math.random()*canvasEl.height/9 + 10, rndRGB1,  rndRGB2);
 }
