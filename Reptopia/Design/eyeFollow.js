c18037e58f046d501fa138926492644fbe2125f2
Lee JungHyeok <wndgur76@naver.com>
2021-01-10 15:33:07 +0900

    function structure changed.
    little update.

--- Reptopia/Design/eyeFollow.js
+++ Reptopia/Design/eyeFollow.js
@@ -23,7 +23,7 @@ setting();
 loop();
 
 function setting(){ //Initial setting
-    for(let temp = 0; temp < 66; temp++)
+    for(let temp = 0; temp < 26; temp++)
         eyes.push(randomEye());
 }
 
@@ -63,16 +63,19 @@ function Eye(x, y, blackRadius, whiteRadius, blackColor, whiteColor, eyelidColor
     this.blackRadius = blackRadius;
 
     this.whiteRadius = whiteRadius;
-    this.blackRadiusB = this.blackRadius; //첫 번째 검은자 백업
-    this.secondBlackRadius = (this.whiteRadius + this.blackRadius) / 2; //첫 번째 검은자를 감싸는 두 번째 검은자 크기
-    if(this.secondBlackRadius > whiteRadius * 3/4) this.secondBlackRadius = whiteRadius * 3/4; //흰자 크기에 따라 검은자 크기 제한
-    this.secondBlackRadiusB = this.secondBlackRadius; //두 번째 검은자 백업
+    this.blackRadiusB = this.blackRadius; //동공 백업
+    this.secondBlackRadius = (this.whiteRadius + this.blackRadius) / 2; //동공 크기
+    if(this.secondBlackRadius > whiteRadius * 3/4) this.secondBlackRadius = whiteRadius * 3/4; //검은자 크기 제한
+    this.secondBlackRadiusB = this.secondBlackRadius; //검은자 백업
 
     //looking 사거리
     this.range = this.secondBlackRadius * 20;
     this.looking = false;
     this.reactingTime = 10 + Math.random()*20;
     this.slowDownCount = 0;
+    
+    //눈꺼풀 두께 변수
+    this.eyelidWidthRadius = 60;
 
     //색 설정
     this.blackColor = arrToRGB(blackColor);
@@ -93,12 +96,12 @@ function Eye(x, y, blackRadius, whiteRadius, blackColor, whiteColor, eyelidColor
                 this.dy *= (this.reactingTime-this.slowDownCount)/this.reactingTime;
                 this.slowDownCount --;
             }
-
-            if(Math.abs(this.dx) > limit) this.looking = false;
-            else this.x += this.dx
-
-            if(Math.abs(this.dy) > limit) this.looking = false;
-            else this.y += this.dy
+            if(distance([this.centerX, this.centerY], [this.x + this.dx, this.y + this.dy]) < limit){
+                if(Math.abs(this.dx) > limit) this.looking = false;
+                else this.x += this.dx
+                if(Math.abs(this.dy) > limit) this.looking = false;
+                else this.y += this.dy
+            }
 
             // --자연스러운 속도제한--
         }
@@ -110,36 +113,36 @@ function Eye(x, y, blackRadius, whiteRadius, blackColor, whiteColor, eyelidColor
             this.y += (this.centerY-this.y)/40;
         }
         else{
-            this.x += (this.centerX-this.x);
-            this.y += (this.centerY-this.y);
+            this.x += (this.centerX-this.x) * 0.98;
+            this.y += (this.centerY-this.y) * 0.98;
         }
     }
 
     this.blinking = false; //깜빡이는지 여부
-    this.blinkStartTime = 0; //깜빡이기 시작한 시간
+    this.blinkStartFrame = 0; //깜빡이기 시작한 시간
     this.blinkEndTime = 0; //깜빡이는 데에 걸리는 총 시간
 
     this.blink = function(frame){ //깜빡임
         if(!this.blinking){
             this.blinking = true;
-            this.blinkStartTime = frame;
+            this.blinkStartFrame = frame;
             this.blinkEndTime = 10 + Math.random()*40;
         }
     }
 
-    this.drawEyelid = function(eyelidWidthRadius){ // 눈꺼풀 그림
-        if(eyelidWidthRadius>180) eyelidWidthRadius = 180;
+    this.drawEyelid = function(){ // 눈꺼풀 그림
+        if(this.eyelidWidthRadius>180) this.eyelidWidthRadius = 180;
         ctx.fillStyle = this.eyelidCol;
 
         //eyelidWidthRadius에 비례한 두께 만큼 눈꺼풀 그림.
-        for(let d=-5; d+eyelidWidthRadius<=185; d+=5){ // 위
+        for(let d=-5; d+this.eyelidWidthRadius<=185; d+=5){ // 위
             ctx.beginPath();
-            ctx.arc(this.centerX, this.centerY, this.whiteRadius+1, degreeToRadian(d), degreeToRadian(d + eyelidWidthRadius), false);
+            ctx.arc(this.centerX, this.centerY, this.whiteRadius+1, degreeToRadian(d), degreeToRadian(d + this.eyelidWidthRadius), false);
             ctx.fill();
         }
-        for(let d=-5; d+eyelidWidthRadius<=185; d+=5){ // 아래
+        for(let d=-5; d+this.eyelidWidthRadius<=185; d+=5){ // 아래
             ctx.beginPath();
-            ctx.arc(this.centerX, this.centerY, this.whiteRadius+1, Math.PI + degreeToRadian(d), Math.PI + degreeToRadian(d+eyelidWidthRadius), false);
+            ctx.arc(this.centerX, this.centerY, this.whiteRadius+1, Math.PI + degreeToRadian(d), Math.PI + degreeToRadian(d+this.eyelidWidthRadius), false);
             ctx.fill();
         }
     }
@@ -164,21 +167,26 @@ function Eye(x, y, blackRadius, whiteRadius, blackColor, whiteColor, eyelidColor
         ctx.fill();
         
         //눈꺼풀 그림
-        this.drawEyelid(70);
+        this.drawEyelid(this.eyelidWidthRadius);
         if(this.blinking){
-            let t = frame - this.blinkStartTime; //깜빡이기 시작한 후 흐른 프레임 수
-            let f = this.blinkEndTime; //깜빡이는데 걸리는 총 프레임 수
-            if(t < f/2){ //눈을 감는 동안
-                this.drawEyelid(t/(f/2) * 180);
-                if(this.blackRadius + 4/f < this.secondBlackRadius) this.blackRadius += 4/f; //검은자 1 크기 조절
-            }
-            else if(t < f){ //눈을 뜨는 동안
-                this.drawEyelid((f-t)/(f/2) * 180);
-                if(this.blackRadius - 8/f > this.blackRadiusB) this.blackRadius -= 8/f; //검은자 1 크기 조절
-                else this.blackRadius = this.blackRadiusB;
-            }
-            else if(t > 1.2*f) {this.blinking = false;}; //눈을 뜨고 난 후 쿨타임
+            let t = frame - this.blinkStartFrame; //깜빡이기 시작한 후 흐른 프레임 수
+            let f = this.blinkEndTime;
+            if(t <= f/2) this.closeEye(t, f/2, 180);
+            else if(t <= f) this.openEye(t, f, 180);
+            else if(t > 1.2*f) this.blinking = false; //눈을 뜨고 난 후 쿨타임
+            else this.eyelidWidthRadius = 60;
         }
+    }
+
+    this.closeEye = function(t, f, widthRadius){
+        this.eyelidWidthRadius = 60 + t/f * widthRadius;
+        if(this.blackRadius + 4/f < this.secondBlackRadius) this.blackRadius += 4/f; //동공 크기 조절
+    }
+
+    this.openEye = function(t, f, widthRadius){
+        this.eyelidWidthRadius = 60 + (f-t)/(f/2) * widthRadius;
+        if(this.blackRadius - 8/f > this.blackRadiusB) this.blackRadius -= 8/f; //동공 크기 조절
+        else this.blackRadius = this.blackRadiusB;
     }
 }
 
@@ -188,14 +196,13 @@ function blinkEyes(){ //마우스 클릭 시 호출
 
 function lookatMe(){ //마우스 더블 클릭 시 호출
     eyes.forEach(eye=>{eye.looking=false;eye.toCenter();});
-    console.log("dbclicked");
 }
 
 function look(event){ //마우스 움직임 시 호출
-    mx = event.pageX+9;
-    my = event.pageY+90;
+    mx = event.pageX + 9;
+    my = event.pageY + 90;
     eyes.forEach(function(eye){
-        if((Math.abs(mx - eye.centerX) < eye.range) & (Math.abs(my - eye.centerY) < eye.range)){ //반응 사거리 구현
+        if((Math.abs(mx - eye.centerX) < eye.range) & (Math.abs(my - eye.centerY) < eye.range)){ //반응 사거리 구현 (정사각형)
             if(!eye.looking){
                 eye.slowDownCount = eye.reactingTime; //반응속도 구현
                 eye.looking = true;
@@ -207,9 +214,9 @@ function look(event){ //마우스 움직임 시 호출
 
 function resize(){ //창 크기 변경 시 호출
     if(canvasEl.width != innerWidth){
-        let wRate = (innerWidth)/canvasEl.width;
-        let hRate = (innerHeight)/canvasEl.height;
-        eyes.forEach(eye => {eye.looking=false; eye.centerX *= wRate;eye.centerY *= hRate;eye.toCenter(1);});
+        let wRate = innerWidth / canvasEl.width;
+        let hRate = innerHeight / canvasEl.height;
+        eyes.forEach(eye => {eye.looking=false; eye.centerX *= wRate; eye.centerY *= hRate; eye.toCenter(1);}); //검은자 강제 고정
         canvasEl.width = innerWidth;
         canvasEl.height = innerHeight;
     }
@@ -226,11 +233,11 @@ function randomEye(){ //무작위 눈 생성
     return new Eye(x, y, blackRadius, whiteRadius, rndRGB1,  rndRGB2, rndRGB3);
 }
 
-function distance(location1, location2){ //location1과 location2 사이의 거리를 계산
+function distance(location1, location2){ //location1 - location2 거리 계산
     return Math.sqrt(((location1[0]-location2[0])**2 + (location1[1] - location2[1])**2));
 }
 
-function arrToRGB(arr){ //배열을 rgb문자열로 변환
+function arrToRGB(arr){ //배열 -> rgb문자열
     let result = "rgb(" + arr[0] + "," + arr[1] + "," + arr[2] + ")";
     return result;
 }
