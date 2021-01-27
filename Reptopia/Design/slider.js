bbcfb7e9f84dc706bfa61b8981c3fcae60ad3192
Lee JungHyeok <wndgur76@naver.com>
2021-01-27 22:38:58 +0900

    1.3.5 21.01.27

--- Reptopia/Design/slider.js
+++ Reptopia/Design/slider.js
@@ -1,52 +1,48 @@
-/*  slider.js
-    2021.01.19
+/*
+    slider.js
+    2021.01.24
     Slider of main categories of Reptopia.
 */
-/*  1.2.4
-        1 Recreated.
+/*
+    1.3.5
+        1 가운데 슬라이드 클릭 시 오작동 해결.
+        2 check(n), createSlide(n, left): 중복 코드 축약.
+        3 hideSlides(), showCreatures(), openStore(), showWorkplace(): Created.
 */
 var beforeN = 1;
 var d = 0;
 var margin = -25;
 var leftA = 0;
+var inner = document.getElementsByClassName("inner")[0];
+
+window.onload = setupSlider;
+
+function setupSlider(){
+    resizeFontSize(innerWidth, innerHeight);
+}
 
 function check(n){
     inner = document.getElementsByClassName("inner")[0];
-    if(n==1){
-        if(beforeN == 3){
-            d = -50;
-            createSlide(2, false);
-            inner.removeChild(inner.children[0]);
-        }
-        else if(beforeN == 2){
-            d = +50;
-            createSlide(3, true);
-            inner.removeChild(inner.children[3]);
-        }
+    if(beforeN == n-1 | beforeN == n+2){
+        d = -50;
+        let Nslide = n+1;
+        if(Nslide == 4) Nslide = 1;
+        createSlide(Nslide, false);
+        inner.removeChild(inner.children[0]);
     }
-    else if(n == 2){
-        if(beforeN == 1){
-            d = -50;
-            createSlide(3, false);
-            inner.removeChild(inner.children[0]);
-        }
-        else if(beforeN == 3){
-            d = +50;
-            createSlide(1, true);
-            inner.removeChild(inner.children[3]);
-        }
+    else if(beforeN == n+1 | beforeN == n-2){
+        d = +50;
+        let Nslide = n-1;
+        if(Nslide == 0) Nslide = 3;
+        createSlide(Nslide, true);
+        inner.removeChild(inner.children[3]);
     }
-    else if(n==3){
-        if(beforeN == 2){
-            d = -50;
-            createSlide(1, false);
-            inner.removeChild(inner.children[0]);
-        }
-        if(beforeN == 1){
-            d = +50;
-            createSlide(2, true);
-            inner.removeChild(inner.children[3]);
-        }
+    else {
+        hideSlides();
+        if(n==1) showCreatures();
+        else if(n==2) openStore();
+        else if(n==3) showWorkplace();
+        return 0;
     }
     beforeN = n;
     leftA -= d;
@@ -55,31 +51,59 @@ function check(n){
 }
 
 function createSlide(n, left = false){ //'n'슬라이드를 left/right side에 생성
-    if(n == 1){
-        newEl = document.createElement("input");
-        newEl.setAttribute("class", "slide slide_1");
-        newEl.setAttribute("type", "button");
-        newEl.setAttribute("value", "Creature");
-        newEl.setAttribute("onclick", "check(1)");
-        if(left) inner.insertBefore(newEl, inner.firstChild);
-        else inner.appendChild(newEl);
+    let newEl = document.createElement("input");
+    let newElAttributes = {"class":"", "type":"", "value":"", "onclick":""};
+    
+    if(n==1) newElAttributes = {"class":"slide slide_1", "value":"Creature", "onclick":"check(1)"};
+    else if(n==2) newElAttributes = {"class":"slide slide_2", "value":"Store", "onclick":"check(2)"};
+    else if(n==3) newElAttributes = {"class":"slide slide_3", "value":"Work", "onclick":"check(3)"};
+    
+    newEl.setAttribute("style", "font-size: "+ String((canvasEl.width+canvasEl.height)/30) + "px;");
+    newEl.setAttribute("type", "button");
+    newEl.setAttribute("class", newElAttributes["class"]);
+    newEl.setAttribute("value", newElAttributes["value"]);
+    newEl.setAttribute("onclick", newElAttributes["onclick"]);
+
+    if(left) inner.insertBefore(newEl, inner.firstChild);
+    else inner.appendChild(newEl);
+}
+
+function resizeFontSize(width, height){
+    inner = document.getElementsByClassName('inner')[0];
+    let sliders = inner.children;
+    for(let i=0; i<sliders.length; i++)
+        sliders[i].setAttribute("style", "font-size: "+ String((width+height)/30) + "px;");
+}
+
+function hideSlides(){
+    let slides = document.getElementById("slides");
+    slides.setAttribute("style", 'top: -15%');
+}
+
+function showCreatures(){
+    cage = document.createElement("canvas");
+    cage.id = "cage";
+
+    cages = document.getElementById("cages");
+    cages.appendChild(cage);
+    cages.setAttribute("style", "top: 5%; height: 90%");
+    cage.setAttribute("style", "height: 30%;");
+
+    var newCtx = cage.getContext("2d");
+    canvasLoop(cage, newCtx);
+
+    function canvasLoop(){
+        newCtx.fillStyle = 'green';
+        newCtx.fillRect(0,0, cage.width, cage.height);
+    
+        requestAnimationFrame(canvasLoop);
     }
-    else if(n == 2){
-        newEl = document.createElement("input");
-        newEl.setAttribute("class", "slide slide_2");
-        newEl.setAttribute("type", "button");
-        newEl.setAttribute("value", "Store");
-        newEl.setAttribute("onclick", "check(2)");
-        if(left) inner.insertBefore(newEl, inner.firstChild);
-        else inner.appendChild(newEl);
-    }
-    else if(n == 3){
-        newEl = document.createElement("input");
-        newEl.setAttribute("class", "slide slide_3");
-        newEl.setAttribute("type", "button");
-        newEl.setAttribute("value", "Work");
-        newEl.setAttribute("onclick", "check(3)");
-        if(left) inner.insertBefore(newEl, inner.firstChild);
-        else inner.appendChild(newEl);
-    }
+}
+
+function openStore(){
+
+}
+
+function showWorkplace(){
+
 }
