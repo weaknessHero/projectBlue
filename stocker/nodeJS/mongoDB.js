7b4e364c6decc6473f39b33316b9051dc157024f
weaknessHero <41188989+weaknessHero@users.noreply.github.com>
2021-05-15 06:54:42 +0900

    Add files via upload

---
+++ stocker/nodeJS/mongoDB.js
@@ -0,0 +1,16 @@
+fs.readFile('./static/corpCodeData.csv','utf8', function(err, data){
+  //data string conversion to arr
+  var des = data.split("\r\n");
+  
+  des.shift();
+  des.pop();
+  
+  var i = des.length;
+  while(i--){
+    des[i]=des[i].split(',');
+  
+    dbo.collection('stockId').insertOne({name:des[i][0],stockCode:des[i][1]}, function(err, db){
+      if(err) throw err;
+    });
+  
+  }
