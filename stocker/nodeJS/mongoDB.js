b85dc5719091767790adcd24913f38e0fbb7fd85
weaknessHero <41188989+weaknessHero@users.noreply.github.com>
2021-05-15 15:33:37 +0900

    Add files via upload

--- stocker/nodeJS/mongoDB.js
+++ stocker/nodeJS/mongoDB.js
@@ -1,16 +1,33 @@
-fs.readFile('./static/corpCodeData.csv','utf8', function(err, data){
-  //data string conversion to arr
-  var des = data.split("\r\n");
-  
-  des.shift();
-  des.pop();
-  
-  var i = des.length;
-  while(i--){
-    des[i]=des[i].split(',');
-  
-    dbo.collection('stockId').insertOne({name:des[i][0],stockCode:des[i][1]}, function(err, db){
-      if(err) throw err;
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+const fs = require('fs');
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  dbo.createCollection('stockId', function(err, res) {
+    fs.readFile('./static/corpCodeData.csv','utf8', function(err, data){
+      //data string conversion to arr
+      var des = data.split("\r\n");
+      
+      des.shift();
+      des.pop();
+      
+      var i = des.length;
+      while(i--){
+        des[i]=des[i].split(',');
+      
+        dbo.collection('stockId').insertOne({name:des[i][0],stockCode:des[i][1]}, function(err, db){
+          if(err) throw err;
+        });
+      
+      }
     });
-  
-  }
+    if (err) throw err;
+    console.log("Collection created!");
+    db.close();
+  });
+});
+
+
